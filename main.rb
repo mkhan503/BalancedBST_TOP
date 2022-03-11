0d67c53994d8c9e7282214a01285f3c4f1bf620d
mkhan503 <mahah.mujeebkhan@gmail.com>
2022-03-11 12:34:35 +0400

    DF BF yield exercises. TODO: Simplify code

---
+++ main.rb
@@ -0,0 +1,31 @@
+require_relative 'lib/node.rb'
+
+
+p tree = Tree.new([35, 36, 38, 45, 47,8])
+
+p tree.preorder
+
+
+=begin 
+node = Node.new(10)
+
+
+tree.insert2(node)
+
+p tree
+
+
+ if target.left_child.nil? 
+        data = target.right_child.data
+        delete(data)
+        target.data = data
+      else
+        target = target.left_child
+        target.left_child = nil
+      end
+=end
+
+
+ 
+
+
